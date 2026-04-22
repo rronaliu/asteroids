ecb4a70473a1a1942c6843b47556f12d6f8284ad
rronaliu <rronaliu55@gmail.com>
2026-04-22 11:04:56 +0200

    wip

--- main.py
+++ main.py
@@ -1,25 +1,33 @@
 import pygame
 from constants import SCREEN_WIDTH, SCREEN_HEIGHT
 from logger import log_state
+from player import Player
 
 def main():
-    print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
-    print(f"Screen width: {SCREEN_WIDTH}, Screen height: {SCREEN_HEIGHT}")
-    
     pygame.init()
-    
+    clock = pygame.time.Clock()
+    dt = 0
+
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+    # Create player in the center of the screen
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
 
     while True:
         log_state()
 
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
-                return 
+                return
 
         screen.fill("black")
 
+
+        # Draw the player
+        player.draw(screen)
+        
         pygame.display.flip()
+        dt = clock.tick(60) / 1000.0
 
 if __name__ == "__main__":
     main()
